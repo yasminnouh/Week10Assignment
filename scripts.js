a92ca0bcce3c0b6bc6be0d7e906707ea6ba1af6e
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-04-12 11:51:47 -0400

    Pulled content from example given last week into files.

---
+++ scripts.js
@@ -0,0 +1,32 @@
+for (var i=0; i<guardianHeroinData.length; i++) {
+	var rowData = guardianHeroinData[i];
+	
+	var $template = $('<div class="quoteContainer">' +
+				'<div class="mainQuote">'+rowData.mainQuote+'</div>' +
+				'<div class="readMore">Read More</div>'+
+				
+				'<div class="moreInfo">'+
+					'<div class="wholeQuote">'+rowData.wholeQuote+'</div>' +
+					'<div class="source">'+rowData.source+'</div>' +
+					'<div class="yearsClean">'+rowData.yearsClean+'</div>' +
+					'<div class="location">'+rowData.location+'</div>' +
+				'</div>'+
+			'</div>');
+			
+			$template.find(".readMore").on("click", toggleContent); 
+			
+			
+			
+			function toggleContent(e) {
+				$quoteContainer = $(this).parent();
+				
+				if ($quoteContainer.hasClass("expanded")) { //quoteContainer has class .expanded (you can see content), so remove content
+					$quoteContainer.removeClass("expanded");
+					
+				} else { //quoteContainer does not have class expanded (you can't see content), so show content!
+					$quoteContainer.addClass("expanded");
+				}
+			}
+			
+			$('#dataContainer').append($template); 
+}
