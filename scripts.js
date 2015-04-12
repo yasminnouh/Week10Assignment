59689e7a2f7b69dc5e03d7047f406fda4032ce32
Yasmin Nouh <yasminnouh@Yasmins-MacBook-Pro.local>
2015-04-12 12:18:55 -0400

    Removed source, yearsClean and location from moreInfo attribute.

--- scripts.js
+++ scripts.js
@@ -7,9 +7,6 @@ for (var i=0; i<guardianHeroinData.length; i++) {
 				
 				'<div class="moreInfo">'+
 					'<div class="wholeQuote">'+rowData.wholeQuote+'</div>' +
-					'<div class="source">'+rowData.source+'</div>' +
-					'<div class="yearsClean">'+rowData.yearsClean+'</div>' +
-					'<div class="location">'+rowData.location+'</div>' +
 				'</div>'+
 			'</div>');
 			
